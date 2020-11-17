12156a625d84978ee52276294711083985eb876d
jlvdb <jlvdb@astro.uni-bonn.de>
2020-11-17 20:45:31 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+with open("requirements.txt") as f:
+    install_requires = [pkg.strip() for pkg in f.readlines() if pkg.strip()]
+
+setuptools.setup(
+    name="tabeval",
+    version="1.0",
+    author="Jan Luca van den Busch",
+    description="Python eval() on key-value-based numerical data structures.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jlvdb/tabeval",
+    packages=setuptools.find_packages(),
+    install_requires=install_requires)
